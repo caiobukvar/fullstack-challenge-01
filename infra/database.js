878146e13019bd40828f0033f8e4aa76422d8c51
Caio <bukvar2009@gmail.com>
2023-12-04 17:52:46 -0300

    feat: instalando client postgresql via sudo - para utilizar com o docker

---
+++ infra/database.js
@@ -0,0 +1,9 @@
+// configuração inicial do pg - direto do site https://node-postgres.com/
+
+import { Client } from "pg";
+const client = new Client();
+await client.connect();
+
+const res = await client.query("SELECT $1::text as message", ["Hello world!"]);
+console.log(res.rows[0].message); // Hello world!
+await client.end();
