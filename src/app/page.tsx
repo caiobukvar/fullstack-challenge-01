df9726a454d1421720bed18a4170a443043edb95
Caio <bukvar2009@gmail.com>
2023-12-08 16:04:59 -0300

    feat: criando o componente de contato

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { Heading } from "@chakra-ui/react";
 import styles from "./page.module.scss";
-import ContactForm from "@/components/ContactForm";
+import ContactForm from "@/components/contactForm";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Heading>Nome do site</Heading>
+      <Heading>Bkvr.DEV</Heading>
       <ContactForm />
     </main>
   );
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -11,14 +11,10 @@ import {
   FormLabel,
   Heading,
   Input,
-  Text,
   Textarea,
   VStack,
-  useToast,
 } from "@chakra-ui/react";
 import { useState } from "react";
-import { sendContactForm } from "../lib/api";
-import database from "../../infra/database";
 
 const initValues = {
   name: "",
@@ -42,10 +38,8 @@ const initState: ContactDataState = {
 };
 
 export default function ContactForm() {
-  const toast = useToast();
   const [contactData, setContactData] = useState(initState);
   const [touched, setTouched] = useState<{ [key: string]: boolean }>({});
-
   const { values, isLoading, error } = contactData;
 
   const handleChange = (
@@ -59,59 +53,21 @@ export default function ContactForm() {
       },
     }));
 
-  type onBlurEvent = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => void;
-
-  const onBlur: onBlurEvent = ({ target }) => {
+  const OnBlurEvent = ({
+    target,
+  }: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
     setTouched((prev) => ({
       ...prev,
       [target.name]: true,
     }));
-  };
-
-  const onSubmit = async () => {
-    setContactData((prev) => ({
-      ...prev,
-      isLoading: true,
-    }));
-
-    try {
-      await sendContactForm(values);
-
-      setTouched({});
-      setContactData(initState);
-
-      toast({
-        title: "E-mail enviado com sucesso!",
-        status: "success",
-        duration: 2000,
-        position: "top",
-      });
-    } catch (error) {
-      const typedError = error as Error;
-
-      setContactData((prev) => ({
-        ...prev,
-        isLoading: false,
-        error: typedError.message,
-      }));
-      toast({
-        title: "Falha ao enviar e-mail. Tente novamente mais tarde!",
-        status: "error",
-        duration: 2000,
-        position: "top",
-      });
-    }
-  };
 
   return (
-    <Card align="center" w="400px">
+    <Card align="center">
       <CardHeader>
-        <Heading size="md"> Entre em contato</Heading>
+        <Heading size="md"> Entre em contato conosco!</Heading>
       </CardHeader>
-      <CardBody w="100%">
-        <VStack gap={2}>
+      <CardBody w="500px">
+        <VStack spacing={2}>
           <FormControl isRequired isInvalid={touched.name && !values.name}>
             <FormLabel>Nome</FormLabel>
             <Input
@@ -119,7 +75,7 @@ export default function ContactForm() {
               placeholder="Insira seu nome"
               value={values.name}
               onChange={handleChange}
-              onBlur={onBlur}
+              onBlur={OnBlurEvent}
             />
             <FormErrorMessage>Preenchimento obrigatório</FormErrorMessage>
           </FormControl>
@@ -128,10 +84,10 @@ export default function ContactForm() {
             <FormLabel>E-mail</FormLabel>
             <Input
               name="email"
-              placeholder="Insira seu e-mail"
+              placeholder="Insira seu email"
               value={values.email}
               onChange={handleChange}
-              onBlur={onBlur}
+              onBlur={OnBlurEvent}
             />
             <FormErrorMessage>Preenchimento obrigatório</FormErrorMessage>
           </FormControl>
@@ -143,7 +99,7 @@ export default function ContactForm() {
               placeholder="Insira seu telefone"
               value={values.phone}
               onChange={handleChange}
-              onBlur={onBlur}
+              onBlur={OnBlurEvent}
             />
             <FormErrorMessage>Preenchimento obrigatório</FormErrorMessage>
           </FormControl>
@@ -152,13 +108,13 @@ export default function ContactForm() {
             isRequired
             isInvalid={touched.message && !values.message}
           >
-            <FormLabel>Mensagem</FormLabel>
+            <FormLabel>Motivo de contato</FormLabel>
             <Textarea
               name="message"
-              placeholder="Insira o motivo de contato"
+              placeholder="Insira o motivo de contato "
               value={values.message}
               onChange={handleChange}
-              onBlur={onBlur}
+              onBlur={OnBlurEvent}
             />
             <FormErrorMessage>Preenchimento obrigatório</FormErrorMessage>
           </FormControl>
@@ -167,11 +123,9 @@ export default function ContactForm() {
       <CardFooter>
         <Button
           colorScheme="blue"
-          isLoading={isLoading}
           isDisabled={
-            !values.name || !values.phone || !values.email || !values.message
+            !values.name || !values.email || !values.phone || !values.message
           }
-          onClick={onSubmit}
         >
           Enviar
         </Button>
